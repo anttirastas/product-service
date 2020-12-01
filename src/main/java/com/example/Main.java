3c13af987a813dd61c0d763d890594abea9ff236
Antti Rastas <antti.rastas@eficode.com>
2020-12-01 09:58:42 +0200

    Add subscriptions and hardcoded values

--- src/main/java/com/example/Main.java
+++ src/main/java/com/example/Main.java
@@ -15,28 +15,17 @@ public class Main {
   }
 
   public static void main(String[] args) throws Exception {
-    /*
     // Listener services use:
     Service.service(new Subscription[]{
-        new Subscription("user-history", "buy-product", (body, sender) -> {
-          System.out.println("user-history: buy-product");
+        new Subscription("product", "fetch-product", (body, sender) -> {
+          System.out.println("product: fetch-product");
+          sender.send("display", "SESSION_ID,product,PRODUCT_ID,Coffee");
         }),
-        new Subscription("user-history", "login", (body, sender) -> {
-          System.out.println("user-history: login");
-          sender.send("buy-product", "Coffee");
+        new Subscription("product", "fetch-product-page", (body, sender) -> {
+          System.out.println("product: fetch-product-page");
+          sender.send("display", "SESSION_ID,product,PRODUCT_ID,Coffee");
         })
     });
-    */
-
-    // Gateway service uses:
-    Subscription.Sender globalSender = Service.service(new Subscription[]{
-        new Subscription("display", "display", (body, sender) -> {
-          System.out.println("display: display");
-          System.out.println(body);
-          // Push message over socket to the user
-        })
-    });
-    globalSender.send("display", "Everything works!");
 
     SpringApplication.run(Main.class, args);
   }
